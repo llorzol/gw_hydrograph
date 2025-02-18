32e12b86955b8e94ade10f64afe369a4f719dee1
llorzol <llorzol@gmail.com>
2025-02-17 20:53:37 -0800

    version 1.03 February 17, 2025

--- htdocs/javascripts/usgs/owrdService.js
+++ htdocs/javascripts/usgs/owrdService.js
@@ -3,8 +3,8 @@
 *
 * owrdService is a JavaScript library to parse the RDB output from OWRD output.
 *
-* version 1.01
-* February 16, 2025
+* version 1.03
+* February 17, 2025
 *
 */
 
@@ -34,7 +34,7 @@
  
 // Retrieve information
 //
-function owrdRequest(nwis_text, nwis_column, data_input) {
+function owrdRequest(siteIdentifier, columnIdentifier, sourceIdentifier) {
     myLogger.info("owrdRequest");
 
     // Build ajax requests
@@ -44,10 +44,10 @@ function owrdRequest(nwis_text, nwis_column, data_input) {
     // Request for site information
     //
     let request_type = "GET";
-    let script_http  = `https://waterservices.usgs.gov/nwis/site/?format=rdb&sites=${nwis_text}&siteOutput=expanded&siteStatus=all`
+    let script_http  = `https://apps.wrd.state.or.us/apps/gw/gw_data_rws/api/${siteIdentifier}/gw_site_summary/?public_viewable=`
     let data_http    = '';
-    let dataType     = "text";
-    myLogger.info(`Site service ${script_http}`);
+    let dataType     = "json";
+    myLogger.info(`OWRD Site service ${script_http}`);
 
     // Web request
     //
@@ -60,10 +60,11 @@ function owrdRequest(nwis_text, nwis_column, data_input) {
             message = "Processed OWRD Site information";
             openModal(message);
             fadeModal(2000);
-            [mySiteRecords, mySiteLegend] = parseSiteOWRD(myData);
+            mySiteRecords = parseSiteOWRD(myData);
         },
         error: function (error) {
-            message = `Failed to load OWRD Site information ${error}`;
+            message = `Failed to load OWRD Site information ${error.statusText}`;
+            myLogger.error(message);
             openModal(message);
             fadeModal(2000);
             return false;
@@ -72,12 +73,12 @@ function owrdRequest(nwis_text, nwis_column, data_input) {
 
     // Request for groundwater information
     //
-    //https://nwis.waterdata.OWRD.gov/nwis/gwlevels?search_site_no=423623121174001&search_parameter_cd=72019&format=rdb&date_format=YYYY-MM-DD&list_of_search_criteria=search_site_no,search_parameter_cd
+    // https://apps.wrd.state.or.us/apps/gw/gw_data_rws/api/KLAM0000588/gw_measured_water_level/?start_date=1/1/1905&end_date=1/1/2025&public_viewable=
     //
     request_type = "GET";
-    script_http  = `https://nwis.waterdata.OWRD.gov/nwis/gwlevels?search_site_no=${nwis_text}&search_site_no_match_type=exact&group_key=NONE&sitefile_output_format=html_table&column_name=agency_cd&column_name=site_no&column_name=station_nm&format=rdb&date_format=YYYY-MM-DD&rdb_compression=value&list_of_search_criteria=search_site_no`
+    script_http  = `https://apps.wrd.state.or.us/apps/gw/gw_data_rws/api/${siteIdentifier}/gw_measured_water_level/?start_date=1/1/1900&end_date=1/1/2030&public_viewable`
     data_http    = '';
-    dataType     = "text";
+    dataType     = "json";
     myLogger.info(`Groundwater service ${script_http}`);
 
     // Web request
@@ -91,10 +92,11 @@ function owrdRequest(nwis_text, nwis_column, data_input) {
             message = "Processed OWRD Groundwater Measurement information";
             openModal(message);
             fadeModal(2000);
-            [myGwRecords, myGwLegend] = parseGwOWRD(myData);
+            myGwRecords = parseGwOWRD(myData);
         },
         error: function (error) {
-            message = `Failed to load OWRD Groundwater Measurement information ${error}`;
+            message = `Failed to load OWRD Groundwater Measurement information ${error.statusText}`;
+            myLogger.error(message);
             openModal(message);
             fadeModal(2000);
             return false;
@@ -106,11 +108,9 @@ function owrdRequest(nwis_text, nwis_column, data_input) {
     $.when.apply($, webRequests).then(function() {
 
         fadeModal(2000);
-        myLogger.info('NWIS output')
+        myLogger.info('OWRD output')
         myLogger.info(mySiteRecords)
-        myLogger.info(mySiteLegend)
         myLogger.info(myGwRecords)
-        myLogger.info(myGwLegend)
 
         buildTablesPanel(mySiteRecords, myGwRecords);
 
@@ -119,222 +119,41 @@ function owrdRequest(nwis_text, nwis_column, data_input) {
 
 }
                           
-function parseSiteOWRD (dataRDB) {
+function parseSiteOWRD (myJson) {
     myLogger.info('parseSiteOWRD');
-    //myLogger.info(dataRDB);
+    myLogger.info(myJson);
 
-   var message = 'Retrieving site information';
-   openModal(message);
-                           
-    // Parse in lines
-    //
-    var fileLines = dataRDB.split(/\r?\n/);
-    myLogger.info('fileLines');
-    myLogger.info(fileLines);
+    let message = 'Retrieving OWRD site information';
+    openModal(message);
 
-    // Filter out comment header lines
-    //
-    let commentLines = fileLines.filter(line => line.startsWith("#") && line.length > 3);
-    myLogger.info('commentLines');
-    myLogger.info(commentLines.join('\n'));
+    let myData = null;
 
-    // Parse from header explanations
+    // Parse for site information
     //
-    let myDataDict      = {};
-
-    let myDataInfo    = /^# The following selected fields are included in this output:/;
-    let myDataInfoEnd = /^#$/;
-    let qualifier     = false;
-     
-    // Skip header lines
-    //
-    while(commentLines.length > 0) {
+    let feature_count = myJson.feature_count;
+    if(feature_count == 1) {
+        myData = [];
+        let myRecord = myJson.feature_list[0];
+        myRecord.coop_site_no = myRecord.gw_logid;
+        myRecord.station_nm = myRecord.usgs_pls_notation_display
+        myRecord.hole_depth_va = myRecord.max_depth;
         
-        let fileLine  = commentLines.shift().trim();
-          
-        // Header portion for data dictionary information
-        //
-        if(myDataInfo.test(fileLine)) { qualifier = true; continue; }
-
-        // Data dictionary portion
-        //
-        if(qualifier) {
-            let Fields        = fileLine.split(/\s+/);
-            //console.log(Fields);
-            var blank         = Fields.shift();
-            var field_name    = Fields.shift();
-            //console.log(field_name);
-            var blank         = Fields.shift();
-            var description   = Fields.join(" ");
-            //console.log(description);
-
-            myDataDict[field_name] = description;
-        }
+        myData.push(myRecord)
+        
+        let myKeys     = Object.keys(myRecord);
+        myData.columns = myKeys
     }
-    myLogger.info('SITE myDataDict');
-    myLogger.info(myDataDict);
-
-    // Filter out comment lines (assuming they start with '#')
-    //
-    let dataLines = fileLines.filter(line => !line.startsWith("#"));
-    myLogger.info('dataLines');
-    myLogger.info(dataLines);
- 
-    // Remove format header line
-    //
-    let formatLine = dataLines.splice(1, 1);
-    myLogger.info('dataLines');
-    myLogger.info(dataLines);
-      
-    // Data lines
-    //
-    myLogger.info(dataLines);
-    myData = d3.tsvParse(dataLines.join('\n'))
-    myLogger.info('SITE');
-    myLogger.info(myData);
-    Fields = myData.columns;
-    myLogger.info(Fields);
     
-    return [ myData, myDataDict ];
-}
+    // Return information
+    //
+    return myData;
+  }
 
-function parseGwOWRD(dataRDB) {
+function parseGwOWRD(myJson) {
     myLogger.info('parseGwOWRD');
-    //myLogger.info(dataRDB);
+    myLogger.info(myJson);
 
     let myData;
-
-    // Split the data into lines
-    //
-    let fileLines = dataRDB.split(/\r?\n/);
-    myLogger.info('fileLines');
-    myLogger.info(fileLines);
-
-    // Filter out comment header lines
-    //
-    let commentLines = fileLines.filter(line => line.startsWith("#") && line.length > 3);
-    myLogger.info('commentLines');
-    myLogger.info(commentLines);
-
-    // Parse from header explanations
-    //
-    let myDataDict      = {};
-
-    let myReferenced    = /^# Referenced /;
-    let myAgencyInfo    = /^# Referenced agency codes/;
-    let mySiteInfo      = /^# Referenced site type codes/;
-    let myReferenceInfo = /^# Referenced vertical datum codes/;
-    let myStatusInfo    = /^# Referenced water-level site status codes/;
-    let myDateAcyInfo   = /^# Referenced water-level date-time accuracy codes/;
-    let myLevAcyInfo    = /^# Referenced water-level accuracy codes/;
-    let myLevSrcInfo    = /^# Referenced source of measurement codes/;
-    let myMethodInfo    = /^# Referenced method of measurement codes/;
-    let myAgingInfo     = /^# Referenced water-level approval-status codes/;
-    let myParameterInfo = /^# The following parameters are included/;
-    
-    let myRe            = /\(\w+\)/;
-    
-    let qualifier       = null;
-
-    // Parse qualifiers from header lines
-    //
-    while(commentLines.length > 0) {
-        
-        let fileLine  = commentLines.shift().trim();
-
-        // Header portion for site status information
-        //
-        if(myReferenced.test(fileLine) || myParameterInfo.test(fileLine)) {
-
-            // Header portion for measuring agency codes information
-            //
-            if(myAgencyInfo.test(fileLine)) { qualifier = 'lev_agency_cd'; }
-
-            // Header portion for site type codes information
-            //
-            if(myAgencyInfo.test(fileLine)) { qualifier = 'site_tp_cd'; }
-
-            // Header portion for referenced vertical datum codes
-            //
-            if(myReferenceInfo.test(fileLine)) { qualifier = 'sl_datum_cd'; }
-
-            // Header portion for lev_status_cds information
-            //
-            if(myStatusInfo.test(fileLine)) { qualifier = 'lev_status_cd'; }
-
-            // Header portion for  water-level date-time accuracy
-            //
-            if(myDateAcyInfo.test(fileLine)) { qualifier = 'lev_dt_acy_cd'; }
-
-            // Header portion for water-level accuracy
-            //
-            if(myLevAcyInfo.test(fileLine)) { qualifier = 'lev_acy_cd'; }
-
-            // Header portion for source of measurement
-            //
-            if(myLevSrcInfo.test(fileLine)) { qualifier = 'lev_src_cd'; }
-
-            // Header portion for method codes
-            //
-            if(myMethodInfo.test(fileLine)) { qualifier = 'lev_meth_cd'; }
-
-            // Header portion for water-level approval-status
-            //
-            if(myAgingInfo.test(fileLine)) { qualifier = 'lev_age_cd'; }
-
-            // Header portion for parameter codes information
-            //
-            if(myParameterInfo.test(fileLine)) { qualifier = 'parameter_cd'; }
-
-            // Data dictionary
-            //
-            myDataDict[qualifier] = {};
-
-            continue;
-        }
-
-        // Parse description
-        //
-        if(qualifier) {
-            let Fields = fileLine.split(/\s+/);
-            let myCode = Fields[1];
-            let myText = Fields.slice(2).join(" ").replace(/^-\s+/,'');
-            myDataDict[qualifier][myCode] = myText;
-        }
-    }
-    myLogger.info('GW myDataDict');
-    myLogger.info(myDataDict);
-
-    // Filter out comment lines (assuming they start with '#')
-    //
-    let tempLines = fileLines.filter(line => !line.startsWith("#"));
-    myLogger.info('tempLines');
-    myLogger.info(tempLines);
-
-    // Set column header line
-    //
-    let columnLine = tempLines[0];
-    myLogger.info('columnLine');
-    myLogger.info(columnLine);
-
-    let dataLines = tempLines.filter(line => line.endsWith("72019"));
-    myLogger.info('dataLines');
-    myLogger.info(dataLines);
-
-    // Add column header line
-    //
-    dataLines.unshift(columnLine)
-    myLogger.info('dataLines');
-    myLogger.info(dataLines);
-      
-    // Data lines
-    //
-    myLogger.info(dataLines);
-    myData = d3.tsvParse(dataLines.join('\n'))
-    myLogger.info('GW');
-    myLogger.info(myData);
-    Fields = myData.columns;
-    myLogger.info(Fields);
 
     // Post-process the measurement dates from UTC to PST
     //
@@ -379,71 +198,88 @@ function parseGwOWRD(dataRDB) {
                         minute: '2-digit'
                       };
 
-    for(let i = 0; i < myData.length; i++) {
-        let myRecord = myData[i];
-        for (let key in myDataDict) {
-            if(myRecord[key] && myDataDict[key][myRecord[key]]) {
-                //myLogger.info(`Key ${key} ${myRecord[key]} ${myDataDict[key][myRecord[key]]}`);
-                myRecord[key] = myDataDict[key][myRecord[key]]
+    // Parse for site information
+    //
+    let feature_count = myJson.feature_count;
+    if(feature_count > 0) {
+        myData = [];
+        let myGwRecords = myJson.feature_list.reverse();
+        let myKeys      = null
+        for(let i = 0; i < myGwRecords.length; i++) {
+
+            let myRecord = myGwRecords[i];
+            if(!myKeys) { myKeys = Object.keys(myRecord); }
+            
+            myRecord.lev_va = myRecord.waterlevel_ft_below_land_surface;
+            myRecord.lev_status_cd = myRecord.measurement_status_desc;
+            
+            
+            // Parse date and build full UTC date
+            //
+            let myDate     = new Date(myRecord.measured_datetime)
+            let lev_tz     = 'PST';
+            let lev_dt_acy = 'Date is accurate to the Year'
+           
+            let myYear     = myDate.getFullYear();
+            let myMonth    = myDate.getMonth();
+            let myDay      = myDate.getDate();
+            let myHour     = myDate.getHours();
+            let myMinute   = myDate.getMinutes();
+            if(!myMonth) { myMonth = 8; }
+            else { lev_dt_acy = 'Date is accurate to the Month'; }
+            if(!myDay) { myDay = 15; }
+            else { lev_dt_acy = 'Date is accurate to the Day'; }
+            if(!myHour) { myHour = 12; }
+            else { lev_dt_acy = 'Date is accurate to the Hour'; }
+            if(!myMinute) { myMinute = 0; }
+            else { lev_dt_acy = 'Date is accurate to the Minute'; }
+            
+            myLogger.info(`Date ${myRecord.measured_datetime} ${lev_dt_acy}   ${myRecord.lev_va} ===> ${myRecord.lev_status_cd} `);
+
+            //let myDate = new Date(Date.UTC(myYear, myMonth - 1, myDay, myHour, myMinute));
+
+            let dataPST = '';
+            if(lev_dt_acy === 'Date is accurate to the Minute') {
+                datePST         = new Intl.DateTimeFormat("en-US", ymdhmOptions).format(myDate);
             }
-        }
+            else if(lev_dt_acy === 'Date is accurate to the Hour') {
+                datePST         = new Intl.DateTimeFormat("en-US", ymdhOptions).format(myDate);
+            }
+            else if(lev_dt_acy === 'Date is accurate to the Day') {
+                datePST         = new Intl.DateTimeFormat("en-US", ymdOptions).format(myDate);
+            }
+            else if(lev_dt_acy === 'Date is accurate to the Month') {
+                datePST         = new Intl.DateTimeFormat("en-US", ymOptions).format(myDate);
+            }
+            else if(lev_dt_acy === 'Date is accurate to the Year') {
+                datePST         = new Intl.DateTimeFormat("en-US", yOptions).format(myDate);
+            }
+            else {
+                datePST         = new Intl.DateTimeFormat("en-US", yOptions).format(myDate);
+            }
+            let lev_dtm = new Intl.DateTimeFormat("en-US",dateOptions).format(myDate);
+            //myLogger.info(`Date ${lev_dt} ${lev_tm} ${lev_dt_acy}   ${lev_dtm} ===> ${datePST} `);
 
-        let lev_dt      = myRecord.lev_dt;
-        let lev_tm      = myRecord.lev_tm;
-        let lev_dt_acy  = myRecord.lev_dt_acy_cd;
-        let lev_tz      = myRecord.lev_tz_cd;
+            // Tooltip
+            //
+            let lev_va      = myRecord.lev_va;
+            let lev_status  = myRecord.lev_status_cd;
+            if(!lev_status || lev_status.length < 1) { lev_status = 'Static'; myRecord.lev_status_cd = 'Static'; }
+            let toolTip     = `Waterlevel: ${lev_va} (${lev_status}) on ${datePST}`
+            myLogger.info(`Date ${myRecord.measured_datetime} ${lev_dt_acy}   ${lev_va}   ${lev_status}`);
 
-        // Parse date and build full UTC date
-        //
-        let [myYear, myMonth, myDay] = lev_dt.split(/-/);
-        let [myHour, myMinute] = lev_tm.split(/:/);
-        if(!myMonth) { myMonth = 8; }
-        if(!myDay) { myDay = 15; }
-        if(!myHour) { myHour = 12; }
-        if(!myMinute) { myMinute = 0; }
-        let myDate = new Date(Date.UTC(myYear, myMonth - 1, myDay, myHour, myMinute));
+            // Set
+            //
+            myRecord.id      = i;
+            myRecord.date    = myDate;
+            myRecord.lev_dtm = lev_dtm;
+            myRecord.tooltip = toolTip;
 
-        if(lev_dt_acy === 'Date is accurate to the Minute') {
-            datePST         = new Intl.DateTimeFormat("en-US", ymdhmOptions).format(myDate);
+            myData.push(myRecord)
         }
-        else if(lev_dt_acy === 'Date is accurate to the Hour') {
-            datePST         = new Intl.DateTimeFormat("en-US", ymdhOptions).format(myDate);
-        }
-        else if(lev_dt_acy === 'Date is accurate to the Day') {
-            datePST         = new Intl.DateTimeFormat("en-US", ymdOptions).format(myDate);
-        }
-        else if(lev_dt_acy === 'Date is accurate to the Month') {
-            datePST         = new Intl.DateTimeFormat("en-US", ymOptions).format(myDate);
-        }
-        else if(lev_dt_acy === 'Date is accurate to the Year') {
-            datePST         = yFormat(myRecord.date);
-        }
-        else {
-            datePST         = yFormat(myRecord.date);
-        }
-        let lev_dtm = new Intl.DateTimeFormat("en-US",dateOptions).format(myDate);
-        //myLogger.info(`Date ${lev_dt} ${lev_tm} ${lev_dt_acy}   ${lev_dtm} ===> ${datePST} `);
-
-        // Tooltip
-        //
-        let lev_va      = myRecord.lev_va;
-        let lev_status  = myRecord.lev_status_cd;
-        if(!lev_status || lev_status.length < 1) { lev_status = 'Static'; myRecord.lev_status_cd = 'Static'; }
-        let toolTip     = `Waterlevel: ${lev_va} (${lev_status}) on ${datePST}`
-        myLogger.info(`Date ${lev_dt} ${lev_tm} ${lev_dt_acy}   ${lev_va}   ${lev_status}`);
-
-        // Set
-        //
-        myRecord.id      = i;
-        myRecord.date    = myDate;
-        myRecord.lev_dtm = lev_dtm;
-        myRecord.tooltip = toolTip;
+        
+        myData.columns = myKeys;
     }
-
-    let Obstructed = myData.filter(line => line.lev_status_cd == 'Obstructed');
-    myLogger.info('myData');
-    myLogger.info(myData);
-    myLogger.info(Obstructed);
     
-    return [ myData, myDataDict ];
+    return myData;
   }
